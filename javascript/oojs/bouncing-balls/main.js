f983b223b0a8df910b9877ec5f8c76ac9c970e5a
Martin <49375775+martincarranza@users.noreply.github.com>
2020-07-24 16:53:06 -0700

    Added funcitonality to -Create and draw canvas -Create and draw bouncing balls -Detect Collisions between bouncing balls

--- javascript/oojs/bouncing-balls/main.js
+++ javascript/oojs/bouncing-balls/main.js
@@ -1,14 +1,106 @@
-// setup canvas
+/* eslint-env browser, es6*/
+/*jslint */
 
+// setup canvas
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-// function to generate random number
-
+// function to generate random number between min and max
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
+
+//Ball Constructor
+//Accepts and X,Y, X velocity, Y velocity, color, and size of ball radius
+function Ball(x, y, velX, velY, color, size){
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+}
+
+//Draws the ball and colors it
+Ball.prototype.draw = function(){
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+//Function to update the X and Y of the ball, also
+//handles the X and Y so the ball does not go out of bounds
+Ball.prototype.update = function(){
+    if((this.x + this.size) >= width){
+        this.velX = -(this.velX);
+    }
+    
+    if((this.x - this.size) <= 0){
+        this.velX = -(this.velX);
+    }
+    
+    if((this.y + this.size) >= height){
+        this.velY = -(this.velY);
+    }
+    
+    if((this.y - this.size) <= 0){
+        this.velY = -(this.velY);
+    }
+    
+    this.x += this.velX;
+    this.y += this.velY;
+}
+
+Ball.prototype.collisionDetect = function() {
+  for (let j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+      }
+    }
+  }
+}
+
+
+//Create an object to hold all the balls,then create 25
+let balls = [];
+while(balls.length < 25){
+    let size = random(10,20);
+    let ball = new Ball(
+        //X and Y must be adjusted with size so the balls are drawn
+        //within the canvas
+        random(0 + size, width-size),
+        random(0+ size, height-size),
+        random(-10,10),
+        random(-10,10),
+        //Randomly generate the color using rgb()
+        'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) + ')',
+        size
+    );
+    
+    balls.push(ball);
+}
+
+function loop(){
+    ctx.fillStyle = 'rgba(0,0,0,.25)';
+    ctx.fillRect(0,0,width,height);
+    
+    for (let i = 0; i < balls.length; i++){
+        balls[i].draw();
+        balls[i].update();
+        balls[i].collisionDetect();
+    }
+    
+    requestAnimationFrame(loop);
+}
+
+loop();
